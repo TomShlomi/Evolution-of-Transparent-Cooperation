f6cda378378ab15ed4ee1f23d0079c2d22698e8c
Tom Shlomi <tomshlomi7@gmail.com>
2022-12-07 10:32:19 -0500

    Now average score is printed; fixed error in get_average_scores where the averaging was done incorrectly

--- evidential_cooperation.py
+++ evidential_cooperation.py
@@ -89,11 +89,11 @@ class Environment:
         return average_scores
 
 # Get average scores
-def get_average_scores(agents_generator, game, death_rate=0.0, numtrials=1000, num_rounds=1000, num_games=1):
+def get_average_scores(agents_generator, game, death_rate=0.0, num_trials=1000, num_rounds=1000, num_games=1):
     average_scores = [0] * num_rounds
     nets = None
 
-    for i in range(numtrials):
+    for i in range(num_trials):
         agents, nets = agents_generator()
 
         # Create and run the environment
@@ -101,6 +101,6 @@ def get_average_scores(agents_generator, game, death_rate=0.0, numtrials=1000, n
         scores = env.run(num_rounds=num_rounds, num_games=num_games, death_rate=death_rate)
         # Add the average scores/100 to the average average scores
         for i in range(len(scores)):
-            average_scores[i] += scores[i]/100
+            average_scores[i] += scores[i]/num_trials
 
     return average_scores, nets
